ebf099a8b8a4f29f94763619ca3f1084c5ff3a97
MaxHacker14 <120554978+MaxHacker14@users.noreply.github.com>
2023-03-27 09:25:53 +0200

    Add files via upload

---
+++ FunPoME1.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class FunPoME1{
+    public static void main(String[] args){
+       
+//         int res1, res2;
+
+//        res1 = suma(10,20);
+//        res2 = suma(4,5);
+
+//        System.out.println("Resultado suma 1: " + res1);
+//        System.out.println("Resultado suma 2: " + res2);
+//     }
+//     private static int suma(int numero1, int numero2){
+//         int suma;
+//         suma = numero1 + numero2;
+//         return suma;
+//     }
+// }
+
+
+
+
+//         int lado;
+//         Scanner entrada = new Scanner(System.in);
+
+//         System.out.print("Escribe el lado del cuadro que quieres dibujar: ");
+//         lado = entrada.nextInt();
+    
+//         dibujaCuadro(lado);
+
+//     }
+
+//     private static void dibujaCuadro(int lado){
+
+//         for(int i = 0; i < lado; i++){
+//             for (int j = 0; j < lado; j++){
+//                 if( j== 0 || i==0|| j== lado -1|| i== lado - 1){
+//                     System.out.println("* ");
+//                 } else {
+//                     System.out.println(" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+
+    // int n1, n2, suma;
+    // Scanner entrada = new Scanner(System.in);
+
+    // n1 = pideNumero(entrada,"Escribe número 1: ");
+    // n2 = pideNumero(entrada,"Escribe número 2: ");
+
+    // suma = sumaNumeros(n1, n2);
+
+    // System.out.println(n1 + " + " + n2 + " = " + suma);
+
+    // entrada.close();
+
+
